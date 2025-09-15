11d9c2e2e3b914e588864de334ec2cac1ad662da
ToanTran <ttt01092005@gmail.com>
2025-09-15 23:53:03 +0700

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
+const navLinks = document.querySelectorAll("header nav a");
+const sections = document.querySelectorAll("section");
+const logoLink = document.querySelector(".logo");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector("header nav");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+});
+
+const activePage = () => {
+  const header = document.querySelector("header");
+  const barsBox = document.querySelector(".bars-box");
+
+  header.classList.remove("active");
+  setTimeout(() => {
+    header.classList.add("active");
+  }, 1100);
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  barsBox.classList.remove("active");
+  setTimeout(() => {
+    barsBox.classList.add("active");
+  }, 1100);
+
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  menuIcon.classList.remove("fa-x");
+  navbar.classList.remove("active");
+};
+
+navLinks.forEach((link, idx) => {
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
+      activePage();
+
+      link.classList.add("active");
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 1100);
+    }
+  });
+});
+
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
+
+    navLinks[0].classList.add("active");
+
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 1100);
+  }
+});
 const resumeBtns = document.querySelectorAll(".resume-btn");
 
 resumeBtns.forEach((btn, idx) => {
@@ -15,4 +76,51 @@ resumeBtns.forEach((btn, idx) => {
 
     resumeDetails[idx].classList.add("active");
   });
+});
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+let index = 0;
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portFolioDetails = document.querySelectorAll(".portfolio-detail");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portFolioDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+
+  portFolioDetails[index].classList.add("active");
+  if (index == 0) {
+    arrowLeft.classList.add("disabled");
+  } else {
+    arrowLeft.classList.remove("disabled");
+  }
+
+  if (index == 3) {
+    arrowRight.classList.add("disabled");
+  } else {
+    arrowRight.classList.remove("disabled");
+  }
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 3) {
+    index++;
+  }
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+  }
+  activePortfolio();
 });
